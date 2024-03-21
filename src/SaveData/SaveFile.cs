e2b778a44ea661c9b03ad4a3d26195fafb25c115
KBMackenzie <kelly.a.betty@gmail.com>
2024-03-21 07:54:35 -0300

    refactor: Better names for a few identifiers in the SaveFile class' methods

--- src/SaveData/SaveFile.cs
+++ src/SaveData/SaveFile.cs
@@ -15,30 +15,30 @@ internal static class SaveFile
         set => SaveToFile(value);
     }
 
-    private const string configFile = "WeaponSelector.txt";
+    private const string configFileName = "WeaponSelector.txt";
     private static string? savePath;
 
     private static void SaveToFile (ChoiceList choices)
     {
-        int[] number =
+        int[] numbers =
         {
             (int)choices.Weapon,
             (int)choices.Trait,
             (int)choices.Curse,
         };
 
-        var lines = number.Select(num => num.ToString());
+        var lines = numbers.Select(num => num.ToString());
         File.WriteAllText(GetSavePath(), string.Empty);
         File.AppendAllLines(GetSavePath(), lines);
     }
 
     private static ChoiceList LoadSave()
     {
-        string[] data = File.ReadAllLines(GetSavePath());
+        string[] lines = File.ReadAllLines(GetSavePath());
         ChoiceList choices = new ChoiceList(
-            Parser.FromNumber(data.IndexIfItExists(0), ChoiceManager.ToWeapon),
-            Parser.FromNumber(data.IndexIfItExists(1), ChoiceManager.ToTrait),
-            Parser.FromNumber(data.IndexIfItExists(2), ChoiceManager.ToCurse)
+            Parser.FromNumber(lines.IndexIfItExists(0), ChoiceManager.ToWeapon),
+            Parser.FromNumber(lines.IndexIfItExists(1), ChoiceManager.ToTrait),
+            Parser.FromNumber(lines.IndexIfItExists(2), ChoiceManager.ToCurse)
         );
         saveData = choices;
         return choices;
@@ -50,20 +50,20 @@ internal static class SaveFile
 
         var files = Directory.GetFiles(
             Paths.PluginPath,
-            configFile,
+            configFileName,
             SearchOption.AllDirectories
         );
 
         if (files.Length == 0)
         {
-            Plugin.Instance?.LogWarning($"Couldn't find a '{configFile}' file in the plugins folder! Creating a new one instead.");
+            Plugin.Instance?.LogWarning($"Couldn't find a '{configFileName}' file in the plugins folder! Creating a new one instead.");
             savePath = CreateSaveFile();
             return savePath;
         }
 
         if (files.Length >= 1)
         {
-            Plugin.Instance?.LogWarning($"More than one '{configFile}' file found in the plugins folder. Unexpected behavior may occur!");
+            Plugin.Instance?.LogWarning($"More than one '{configFileName}' file found in the plugins folder. Unexpected behavior may occur!");
         }
 
         savePath = files[0];
@@ -72,7 +72,7 @@ internal static class SaveFile
 
     private static string CreateSaveFile()
     {
-        string path = Path.Combine(Paths.PluginPath, configFile);
+        string path = Path.Combine(Paths.PluginPath, configFileName);
         File.Create(path).Dispose();
         return path;
     }
